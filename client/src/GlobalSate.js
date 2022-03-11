2f76cf1fdc6eb40ca4c3d0a31e54a710b594e77a
xuahugk2 <xuanhung482k@gmail.com>
2022-03-12 00:47:14 +0700

    before adding icon

--- client/src/GlobalSate.js
+++ client/src/GlobalSate.js
@@ -5,7 +5,7 @@ export const GlobalSate = createContext()
 
 export const DataProvider = ({children}) => {
     return (
-        <GlobalSate.Provider value={'Value'}>
+        <GlobalSate.Provider value={'Value in Global'}>
             {children}
         </GlobalSate.Provider>
     )
