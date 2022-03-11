03db81a0d72a4a8840d1bb24ddc1037c35ccb3fd
xuahugk2 <xuanhung482k@gmail.com>
2022-03-11 18:10:45 +0700

    server successfull

--- controller/productController.js
+++ controller/productController.js
@@ -1,11 +1,64 @@
+import { LogTimings } from 'concurrently'
 import productModel from '../models/productModel.js'
+
+class APIfeatures {
+    constructor(query, queryString) {
+        this.query = query
+        this.queryString = queryString
+    }
+
+    filtering(){
+        const queryObj = {...this.queryString}
+        
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete(queryObj[el]))
+
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        //gte = greater than or equal
+        //gt = greater than
+        //lte = lesser than or equal
+        //lt = lesser than
+        this.query.find(JSON.parse(queryStr))
+
+        return this
+    }
+    sorting(){
+        if(this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        } else {
+            this.query = this.query.sort('-createAt')
+        }
+
+        return this
+    }
+    paginating(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 3
+        const skip = (page - 1) * limit
+
+        this.query = this.query.skip(skip).limit(limit)
+
+        return this
+    }
+}
 
 const productController = {
     getProducts: async(req, res) => {
         try {
-            const products = await productModel.find()
+            console.log(req.query);
+            const features = new APIfeatures(productModel.find(), req.query)
+                .filtering().sorting()
 
-            res.json(products)
+            const products = await features.query
+
+            res.json({
+                status: 'success',
+                result: products.length,
+                products: products
+            })
         } catch (error) {
            return res.status(500).json({msg: error.message}) 
         }
--- models/productModel.js
+++ models/productModel.js
@@ -41,6 +41,8 @@ const productSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+}, {
+    timestamps: true
 })
 
 export default mongoose.model('Products', productSchema)
