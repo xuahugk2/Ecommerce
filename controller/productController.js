1a08e5163e6b84dd0c9d944b8046d2cd7c276fbc
xuahugk2 <xuanhung482k@gmail.com>
2022-03-11 15:36:38 +0700

    update and delete product

--- controller/productController.js
+++ controller/productController.js
@@ -14,7 +14,8 @@ const productController = {
         try {
             const {product_id, title, price, description, content, images, category} = req.body
 
-            if(!images) res.status(400).json({msg: 'No images uploaded.'})
+            if(!images) 
+                return res.status(400).json({msg: 'No images uploaded.'})
 
             const product = await productModel.findOne({product_id})
 
@@ -40,14 +41,25 @@ const productController = {
     },
     deleteProduct: async(req, res) => {
         try {
-            
+            await productModel.findByIdAndDelete(req.params.id)
+
+            res.json({msg: "Deleted a Product"})
         } catch (error) {
            return res.status(500).json({msg: error.message}) 
         }
     },
     updateProduct: async(req, res) => {
         try {
-            
+            const {title, price, description, content, images, category} = req.body
+
+            if(!images)
+                return res.status(400).json({msg: 'No images uploaded.'})
+
+            await productModel.findByIdAndUpdate({_id: req.params.id}, {
+                title: title.toLowerCase(), price, description, content, images, category
+            })
+
+            res.json({msg: "Updated a product"})
         } catch (error) {
            return res.status(500).json({msg: error.message}) 
         }
