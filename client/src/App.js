a54e42c21e70b77bfa37adc5f73d69b8ab56166d
xuahugk2 <xuanhung482k@gmail.com>
2022-03-13 01:52:07 +0700

    finished header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalSate'
-import Header from './components/header/Header'
+import Header from './components/headers/Header'
 import MainPages from './components/mainpages/Pages'
 
 function App() {
--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/NotFound/NotFound'
 
 export default function Pages() {
     return(
-        <div>
-            Main Pages Components
-        </div>
+        <Routes>
+            <Route path='/' element={<Products/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/cart' element={<Cart/>} />
+
+            <Route path='*' element={<NotFound/>} />
+        </Routes>
     )
 }
