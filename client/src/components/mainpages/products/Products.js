525c60960e5215e27c539fee496ed4b1342be2ed
xuahugk2 <xuanhung482k@gmail.com>
2022-03-14 20:19:16 +0700

    add loading animation

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,19 +1,23 @@
 import React, {useContext} from 'react'
 import {GlobalSate} from '../../../GlobalSate'
 import ProductItem from '../utils/productItem/ProductItem'
+import Loading from '../utils/loading/Loading'
 
 export default function Products() {
-  const state = useContext(GlobalSate)
+	const state = useContext(GlobalSate)
 
-  const [products] = state.productsAPI.products
-  
-  return (
-	  <div className='products'>
-      {
-        products.map(product => {
-          return <ProductItem key={product._id} product={product}/>
-        })
-      }
-    </div>
-  )
+	const [products] = state.productsAPI.products
+	
+	return (
+		<React.Fragment>
+			<div className='products'>
+				{
+					products.map(product => {
+						return <ProductItem key={product._id} product={product}/>
+					})
+				}
+			</div>
+			{products.length === 0 && <Loading/>}
+		</React.Fragment>
+	)
 }
