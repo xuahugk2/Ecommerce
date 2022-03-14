fa5b8e582491434b0cd138abb4e23c723f99a67c
xuahugk2 <xuanhung482k@gmail.com>
2022-03-14 20:54:48 +0700

    login and register page

--- client/src/components/mainpages/auth/Login.js
+++ client/src/components/mainpages/auth/Login.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export default function Login() {
-  return (
-    <div>Login Page</div>
-  )
+	const [user, setUser] = useState({
+		email: '',
+		password: ''
+	})
+
+	const onChangeInput = (e) => {
+		const {name, value} = e.target
+
+		setUser({...user, [name]:value})
+	}
+
+	const loginSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			await axios.post('/user/login', {...user})
+
+			localStorage.setItem('firstLogin', true)
+
+			window.location.href = "/"
+		} catch (error) {
+			alert(error.response.data.msg)
+		}
+	}
+
+	return (
+		<div className='auth-page'>
+			<form onSubmit={loginSubmit}>
+				<h2>Login</h2>
+
+				<input type="email" name="email" id="email" required placeholder='Email' value={user.email} onChange={onChangeInput}/>
+				
+				<input type="password" name="password" id="password" required autoComplete='on' placeholder='Password' value={user.password} onChange={onChangeInput}/>
+
+				<div className="row">
+					<button type='submit'>Login</button>
+					<Link to='/register'>Register</Link>
+				</div>
+			</form>
+		</div>
+	)
 }
--- client/src/components/mainpages/auth/Register.js
+++ client/src/components/mainpages/auth/Register.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export default function Register() {
-  return (
-    <div>Register Page</div>
-  )
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		password: ''
+	})
+
+	const onChangeInput = (e) => {
+		const {name, value} = e.target
+
+		setUser({...user, [name]:value})
+	}
+
+	const registerSubmit = async (e) => {
+		e.preventDefault()
+
+		try {
+			await axios.post('/user/register', {...user})
+
+			localStorage.setItem('firstLogin', true)
+
+			window.location.href = "/"
+		} catch (error) {
+			alert(error.response.data.msg)
+		}
+	}
+
+	return (
+		<div className='auth-page'>
+			<form onSubmit={registerSubmit}>
+				<h2>Register</h2>
+
+				<input type="text" name="name" id="name" required placeholder='Name' value={user.name} onChange={onChangeInput}/>
+				
+				<input type="email" name="email" id="email" required placeholder='Email' value={user.email} onChange={onChangeInput}/>
+				
+				<input type="password" name="password" id="password" required autoComplete='on' placeholder='Password' value={user.password} onChange={onChangeInput}/>
+
+				<div className="row">
+					<button type='submit'>Register</button>
+					<Link to='/login'>Login</Link>
+				</div>
+			</form>
+		</div>
+	)
 }
